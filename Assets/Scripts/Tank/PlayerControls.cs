0de537167668cd67216427642f7ce729e62010eb
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-05-28 22:46:45 +0300

    initial

---
+++ Assets/Scripts/Tank/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    // public variables can be accessed in Unity editor
+    public float movementSpeed;
+    public float turningSpeed;
+    private Rigidbody rb;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() // 
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        if (inputHorizontal != 0)
+        {
+            Vector3 turning = Vector3.up * inputHorizontal * turningSpeed;    
+            rb.angularVelocity = turning;    // rotates left and right
+        }
+
+        if (inputVertical != 0)
+        {
+            Vector3 movement = transform.forward * inputVertical * movementSpeed;
+            rb.velocity = movement;
+        }
+        
+    }
+}
