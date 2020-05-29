af28b59e18bbc4f2cf5018900b1850e62965b7f0
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-05-29 15:09:51 +0300

    mouse follow part 1

--- Assets/Scripts/Tank/PlayerControls.cs
+++ Assets/Scripts/Tank/PlayerControls.cs
@@ -8,13 +8,19 @@ public class PlayerControls : MonoBehaviour
     // public variables can be accessed in Unity editor
     public float movementSpeed;
     public float turningSpeed;
-    private Rigidbody rb;
+    public float turretTurningSpeed;
     
+    private Rigidbody rb;
+    private Camera mainCamera;
+    private float maxRayDistance = 100f;
+    private int floorMask;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        floorMask = LayerMask.GetMask("Floor");
     }
 
     // Update is called once per frame
@@ -23,17 +29,28 @@ public class PlayerControls : MonoBehaviour
         float inputHorizontal = Input.GetAxis("Horizontal");
         float inputVertical = Input.GetAxis("Vertical");
 
+        // turning left and right
         if (inputHorizontal != 0)
         {
             Vector3 turning = Vector3.up * inputHorizontal * turningSpeed;    
-            rb.angularVelocity = turning;    // rotates left and right
+            rb.angularVelocity = turning;   
         }
 
+        // moving forw/backw
         if (inputVertical != 0)
         {
             Vector3 movement = transform.forward * inputVertical * movementSpeed;
             rb.velocity = movement;
         }
-        
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);    // ray pointing towards mouse cursor
+        RaycastHit hit;    // point where ray hits
+
+        if (Physics.Raycast(ray, out hit, maxRayDistance, floorMask))
+        {
+            // Vextor3
+        }
+
+
     }
 }
