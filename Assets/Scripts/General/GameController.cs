2c7ec27c28dc8b2f92eb9e23db403667819fb9df
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-05-31 13:07:00 +0300

    spawn scripts added

---
+++ Assets/Scripts/General/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public Spawner spawner;
+    public float score;
+    public int lives;
+    public float scorePerTank;
+    public int enemyStartingAmount;
+    public int maxEnemiesAmount;
+
+    private int currentLives;
+    private int currentEnemyAmount;
+    private GameObject player;
+    
+    void Start()
+    {
+        for (int i = 0; i < enemyStartingAmount; i++)
+        {
+            spawner.SpawnEnemy();
+        }
+        
+        
+        
+        spawner.SpawnPlayer();
+        
+        score = 0f;
+        currentLives = lives;
+        currentEnemyAmount = enemyStartingAmount;
+    }
+
+    public void addScore()
+    {
+        score += scorePerTank;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
